f6cd15acf8033294c226cf1652c06b32e2f3b405
EricJobs3 <90584009+EricJobs3@users.noreply.github.com>
2022-09-16 15:58:35 +0930

    change AWS_REGION

--- example/vars.sh
+++ example/vars.sh
@@ -4,7 +4,7 @@
 export TF_STATE_BUCKET="eric-bucket-jenkins" # update your details
 export TF_STATE_OBJECT_KEY="serverless-jenkins-eric.tfstate" # update your details
 export TF_LOCK_DB="eric-lock-DB" # update your details
-export AWS_REGION="ap-southeast-1" # update your details
+export AWS_REGION="ap-southeast-2" # update your details
 
 PRIVATE_SUBNETS='["subnet-0e1f40b9dccd6cb9e", "subnet-04cb04ada9b8b5542"]' # update your details
 PUBLIC_SUBNETS='["subnet-07b0b0052f27b0392", "subnet-0634bad88e263ba4e"]' # update your details
